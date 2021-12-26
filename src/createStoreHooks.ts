980eda4560186ce83b6bfe057dc4e56e2529b395
Shahriar Shojib <shahriar_shojib@hotmail.com>
2021-12-26 18:25:33 +0600

    updated return type of createStoreHooks

--- src/createStoreHooks.ts
+++ src/createStoreHooks.ts
@@ -1,6 +1,11 @@
 import { ReturnStoreType, StateConstraint } from '@poly-state/poly-state';
 import { useStore, useStoreSelector } from './index';
 
-export const createStoreHooks = <T extends StateConstraint>(store: ReturnStoreType<T>) => {
+type UseStore<T> = () => T;
+type UseStoreSelector<T> = (key: keyof T) => T[keyof T];
+
+export const createStoreHooks = <T extends StateConstraint>(
+	store: ReturnStoreType<T>
+): Readonly<[UseStore<T>, UseStoreSelector<T>]> => {
 	return [() => useStore(store), (key: keyof T) => useStoreSelector(store, key)];
 };
