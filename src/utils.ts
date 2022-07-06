34a36f341e452f1d8d75c96ee933f1fcfa346bae
Shahriar Shojib <shahriar_shojib@hotmail.com>
2022-07-06 08:43:45 +0600

    fix: fixed build fix: remove unused files

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector';
-
-export const notInitialized = () => {
-	throw new Error('uSES not initialized!');
-};
-
-export type uSESWS = typeof useSyncExternalStoreWithSelector;
